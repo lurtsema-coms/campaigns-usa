121929206a6f701b2ec953c37ecd7228c64651f7
GabQuing <gab.quing29@gmail.com>
2024-06-26 08:04:05 +0800

    fix login problem

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -32,8 +32,7 @@ new #[Layout('layouts.guest')] class extends Component
     <form wire:submit="login">
 
         <div class="mb-6">
-            <h1 class="text-2xl  text-indigo-950 text-center sm:text-start">WELCOME TO</h1>
-            <h1 class="text-5xl font-bold text-indigo-950 mb-5 text-center sm:text-start">Campaign Academy</h1>
+
             <span class="font-bold">Doesn't have an account yet? <a href="/register" class="text-indigo-950 underline font-bold">Sign Up</a></span>
         </div>
 
@@ -47,21 +46,21 @@ new #[Layout('layouts.guest')] class extends Component
         <!-- Password -->
         <div class="mt-4 ">
             <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input wire:model="form.password" id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-error :messages="$errors->get('form.password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
         <div class=" mt-4 flex flex-col sm:flex-row gap-x-4 ">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded shadow-sm border-indigo-600 focus:border-indigo-600 dark:focus:border-indigo-600 focus:ring-indigo-600 dark:focus:ring-indigo-600" name="remember">
+                <input wire:model="form.remember" id="remember" type="checkbox" class="rounded shadow-sm border-indigo-600 focus:border-indigo-600 dark:focus:border-indigo-600 focus:ring-indigo-600 dark:focus:ring-indigo-600" name="remember">
                 <span class="ms-2 text-sm text-indigo-950">{{ __('Remember me') }}</span>
             </label>
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-indigo-950 hover:text-indigo-900 dark:hover:text-indigo-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-indigo-500 dark:focus:ring-offset-indigo-500" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-indigo-950 hover:text-indigo-900 dark:hover:text-indigo-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-indigo-500 dark:focus:ring-offset-indigo-500" href="{{ route('password.request') }}" wire:navigate>
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
