31f104ce2efda17700cb04467f690d6cd41a7af9
unknown <gab.quing29@gmail.com>
2024-09-10 06:13:55 +0800

    working on the component

---
+++ resources/views/livewire/frontend/cart/cart-notif.blade.php
@@ -0,0 +1,32 @@
+<div class="overflow-y-auto max-h-96 scrollbar-thin scrollbar-thumb-cyan scrollbar-thumb-rounded">
+    <div class="flex cursor-pointer hover:bg-gray-100">
+        <div class="flex justify-center  items-center p-2">
+            <img class="h-auto w-[10rem]" src="{{ asset('frontend/joe.png') }}" alt="">
+        </div>
+        <div class="p-2 text-sm">
+            <p class="font-bold text-[#0E092C]">React - The Complete Guide 2024 (incl. Next.js, Redux)</p>
+            <p class=" text-xs">Created By: Gabriel Camerone A. Quing</p>
+            <p class="font-bold text-[#0E092C]">$4000</p>
+        </div>
+    </div>
+    <div class="flex cursor-pointer hover:bg-gray-100">
+        <div class="flex justify-center  items-center p-2">
+            <img class="h-auto w-[10rem]" src="{{ asset('frontend/tiktok.png') }}" alt="">
+        </div>
+        <div class="p-2 text-sm">
+            <p class="font-bold text-[#0E092C]">React - The Complete Guide 2024 (incl. Next.js, Redux)</p>
+            <p class=" text-xs">Created By: Gabriel Camerone A. Quing</p>
+            <p class="font-bold text-[#0E092C]">$4000</p>
+        </div>
+    </div>
+    <div class="flex cursor-pointer hover:bg-gray-100">
+        <div class="flex justify-center  items-center p-2">
+            <img class="h-auto w-[10rem]" src="{{ asset('frontend/test.png') }}" alt="">
+        </div>
+        <div class="p-2 text-sm">
+            <p class="font-bold text-[#0E092C]">React - The Complete Guide 2024 (incl. Next.js, Redux)</p>
+            <p class=" text-xs">Created By: Gabriel Camerone A. Quing</p>
+            <p class="font-bold text-[#0E092C]">$4000</p>
+        </div>
+    </div>
+</div>
