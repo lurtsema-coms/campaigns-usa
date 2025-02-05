8c6fcde8f3a70b1b68b2fc6916bd1f5163a1dd01
GabQuing <gab.quing29@gmail.com>
2025-02-05 10:21:47 +0800

    model and migration file for likes

---
+++ database/migrations/2025_01_22_213514_create_likes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Comment::class);
+            $table->foreignIdFor(User::class);
+            $table->integer('created_by')->length(10)->unsigned()->nullable();
+            $table->timestamps();
+            $table->unique(['comment_id','user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('likes');
+    }
+};
